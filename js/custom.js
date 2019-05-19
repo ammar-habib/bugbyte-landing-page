e1b299a0a1b541adcc233acbd708c0b732f09d69
ammar-habib <ammar.habib.mughal@gmail.com>
2019-05-19 22:52:51 +0500

    landing-page

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,17 @@
+var totalItems = $('.carousel-item').length;
+var currentIndex = $('.carousel-item.active').index() + 1;
+$('.num').html('0'+currentIndex+'<sub>/ 0'+totalItems+'</sub>');
 
+
+$('#carouselExampleIndicators').on('slide.bs.carousel', function() {
+    currentIndex = $('.carousel-item.active').index() + 1;
+    $('.num').html('0'+currentIndex+'<sub>/ 0'+totalItems+'</sub>');
+    // $('.current-num').text(currentIndex);
+    // $('.total-num').text(totalItems);
+});
+
+
+// This triggers after each slide change
 
 
 
